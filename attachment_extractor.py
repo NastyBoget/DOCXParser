d3c02ded582eebffa2fbde4708ecae75d3d15581
Nasty <bogatenkova.anastasiya@mail.ru>
2020-10-30 17:28:54 +0300

    research in progress

--- attachment_extractor.py
+++ attachment_extractor.py
@@ -78,7 +78,7 @@ class DocxAttachmentsExtractor:
 
                     elif namefile.endswith('.bin'):
                         # extracting PDF-files
-                        ole = olefile.OleFileIO(zfile.open(attachment))
+                        ole = olefile.OleFileIO(zfile.open(attachment).read())
                         if ole.exists("CONTENTS"):
                             data = ole.openstream('CONTENTS').read()
                             if data[0:5] == b'%PDF-':
